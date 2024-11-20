2c49beb142a0d3424a66b40dd34d6dbef9b2a026
ervin-sungkono <ervinsung@gmail.com>
2024-11-20 21:14:10 +0700

    Add form validation on pertemuan 1

--- Pertemuan 1/script.js
+++ Pertemuan 1/script.js
@@ -3,7 +3,34 @@ const exampleForm = document.getElementById('form-example')
 function onSubmitForm(e){
     e.preventDefault()
 
-    const formData = new FormData(exampleForm)
-
-    console.log(formData)
+    new FormData(exampleForm)
 }
+
+function handleSubmitForm(e){
+    const data = e.formData
+    const text = data.get('text')
+    const password = data.get('password')
+    const email = data.get('email')
+
+    let errorMessage = []
+    if(!text){
+        errorMessage.push("Text is required")
+    }
+    if(!password){
+        errorMessage.push("Password is required")
+    }
+    else if(password.length < 8){
+        errorMessage.push("Password length must be at least 8")
+    }
+    if(!email){
+        errorMessage.push("Email is required")
+    }
+    if(errorMessage){
+        alert(errorMessage.join('\n'))
+    }else{
+        // No error, send the form data
+    }
+}
+
+exampleForm.addEventListener("submit", onSubmitForm)
+exampleForm.addEventListener("formdata", handleSubmitForm)
